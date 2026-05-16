abe99ed078af6b601f8b07b8f457274a30665ae9
Akchiche-Mohamed-Aymen <akchiche.mohamedaymen@gmail.com>
2026-05-16 21:13:18 +0200

    add requirements file

--- ai.py
+++ ai.py
@@ -1,6 +1,8 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from key import GEMINI_KEY
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+GEMINI_KEY = os.getenv("GEMINI_KEY")
 chat =  ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=1.0,  # Gemini 3.0+ defaults to 1.0
